688f3a98d374a8374d6b440e657d5ee26559031e
JennyPersichini <nenny1096@gmail.com>
2023-04-19 15:09:09 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop-composizione/Negozio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop_composizione
+{
+    public class Negozio
+    {
+        //Lo shop deve avere un nome, una citta, un indirizzo, un numero civico
+        private string nome;
+        private string città;
+        private string indirizzo;
+        private int numeroCivico;
+
+        private List<Prodotto> prodotti;
+    }
+
+}
