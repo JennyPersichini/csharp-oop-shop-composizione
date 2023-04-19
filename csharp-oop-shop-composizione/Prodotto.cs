c2e81a330cbafb8262995904c2cda8080816bf87
JennyPersichini <nenny1096@gmail.com>
2023-04-19 16:36:39 +0200

    Add prodotto

--- csharp-oop-shop-composizione/Prodotto.cs
+++ csharp-oop-shop-composizione/Prodotto.cs
@@ -13,5 +13,48 @@ namespace csharp_oop_shop_composizione
         private double prezzo;
         private double iva;
 
+        public Prodotto(string nome, string descrizione, double prezzo, double iva)
+        {
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        public string GetDescrizione()
+        {
+            return this.descrizione;
+        }
+
+        public double GetPrezzo()
+        {
+            return this.prezzo;
+        }
+
+        public double getPrezzoConIva()
+        {
+            return prezzo + (prezzo / 100 * iva);
+        }
+
+        public void CambiaPrezzo(double prezzo)
+        {
+            this.prezzo = prezzo;
+        }
+
+        public string GetProdottoStringa()
+        {
+            string rapprStringa = "Nome: " + this.nome + ", ";
+            rapprStringa += "Descrizione: " + this.descrizione + ", ";
+            rapprStringa += "Prezzo: " + this.prezzo + ", ";
+
+            return rapprStringa;
+        }
+
     }
+
 }
