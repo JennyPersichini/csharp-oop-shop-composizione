9f6d2ba9a78ba7762d16cac3735e7f0714b1a528
JennyPersichini <nenny1096@gmail.com>
2023-04-19 15:40:38 +0200

    add get

--- csharp-oop-shop-composizione/Negozio.cs
+++ csharp-oop-shop-composizione/Negozio.cs
@@ -15,6 +15,45 @@ namespace csharp_oop_shop_composizione
         private int numeroCivico;
 
         private List<Prodotto> prodotti;
-    }
 
+        public void Shop(string nome, string città, string indirizzo, int numeroCivico)
+        {
+            this.nome = nome;
+            this.città = città;
+            this.indirizzo = indirizzo;
+            this.numeroCivico = numeroCivico;
+
+            this.prodotti = new List<Prodotto>();
+        }
+
+        //si possano chiedere ognuna delle informazioni contenute nel negozio
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        public string GetCittà()
+        {
+            return this.città;
+        }
+
+        public string GetIndirizzo()
+        {
+            return this.indirizzo;
+        }
+
+        public int GetNumeroCivico()
+        {
+            return this.numeroCivico;
+        }
+
+        public List<Prodotto> GetProdotti()
+        {
+            return this.prodotti;
+        }
+
+        //si possa modificare alcune delle informazioni dello shop
+
+    }
 }
